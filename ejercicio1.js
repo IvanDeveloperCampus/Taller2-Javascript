2f608af39470ee100f1fec93d107822f18309516
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-03-09 08:26:52 -0500

    feat:

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,3 +1,6 @@
-
+/**1. Construir el algoritmo para un programa que ingrese tres
+notas de un alumno, si el promedio es menor o igual a 3.9
+mostrar un mensaje "Estudie“, de lo contrario un mensaje que
+diga "becado" */
 
 
