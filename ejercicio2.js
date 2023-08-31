b7af120cb59dffa6038c57dbf699a0a6aa3d872e
IvanSanchezDev <ivancampus2023@gmail.com>
2023-08-31 09:11:19 -0500

    test: :test_tube: correccion test

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,15 +1,14 @@
 /**2. Dado un número indicar si es par o impar y si es mayor de 10. */
 
-export function esPar(num) {
+export const esPar=(num) =>{
     return num % 2 === 0;
   }
   
-  export function esMayorQue10(num) {
+  export const esMayorQue10=(num)=> {
     return num > 10;
   }
   
-  export function imprimirResultados(num) {
-    console.log(`El num ${num} es ${esPar(num) ? 'par' : 'impar'} y es ${esMayorQue10(num) ? 'mayor' : 'menor'} que 10`);
+  export const imprimirResultados=(num)=> {
+    return `El num ${num} es ${esPar(num) ? 'par' : 'impar'} y es ${esMayorQue10(num) ? 'mayor' : 'menor'} que 10`;
   }
   
-  imprimirResultados(33);
